dbb599c49232853803a58caa914e7efc7ba353d0
Alexandre Magedanz <alexandre.magedanz@gmail.com>
2025-12-18 22:33:33 -0300

    Concluido o Capitulo 10

--- api/operacoes.js
+++ api/operacoes.js
@@ -7,33 +7,53 @@ const {
 } = require("../services/operacoes")
 
 function apiSomar(req, res) {
+    try {   
     const {num1, num2} = req.body
-    const result = somar(num1,num2)
-    res.status(200).send({ result })
+        const result = somar(num1,num2)
+        res.status(200).send({ result })
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
 }
 
 function apiSubtrair(req, res) {
-    const {num1, num2} = req.body
-    const result = subtrair(num1,num2)
-    res.status(200).send({ result })
+    try {
+        const {num1, num2} = req.body
+        const result = subtrair(num1,num2)
+        res.status(200).send({ result })
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
 }
 
 function apiDividir(req, res) {
-    const {num1, num2} = req.body
-    const result = dividir(num1,num2)
-    res.status(200).send({ result })
+    try {
+        const {num1, num2} = req.body
+        const result = dividir(num1,num2)
+        res.status(200).send({ result })
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
 }
 
 function apiMultiplicar(req, res) {
-    const {num1, num2} = req.body
-    const result = multiplicar(num1,num2)
-    res.status(200).send({ result })
+    try {
+        const {num1, num2} = req.body
+        const result = multiplicar(num1,num2)
+        res.status(200).send({ result })
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
 }
 
 function apiMedia(req, res) {
-    const {num1, num2} = req.body
-    const result = media(num1,num2)
-    res.status(200).send({ result })
+    try {
+        const {num1, num2} = req.body
+        const result = media(num1,num2)
+        res.status(200).send({ result })
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
 }
 
 module.exports = {
--- services/operacoes.js
+++ services/operacoes.js
@@ -1,20 +1,38 @@
 function somar(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error('Os valores devem ser numéricos')
+    }
     return num1 + num2
 }   
 
 function subtrair(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error('Os valores devem ser numéricos')
+    }
     return num1 - num2
 } 
 
 function dividir(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error('Os valores devem ser numéricos')
+    }
+    if (num2 === 0) {
+        throw new Error('Não é possível dividir por zero')
+    }
     return num1 / num2
 } 
 
 function multiplicar(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error('Os valores devem ser numéricos')
+    }
     return num1 * num2
 } 
 
 function media(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error('Os valores devem ser numéricos')
+    }
     return ((num1 + num2) / 2)
 } 
 
